387f9dfd165f90ac834ad06ad0fab98aec92b14f
Neil Smith <s3nt1n3lz18@gmail.com>
2021-12-17 17:27:13 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: null,
+    rootValue: {}
+}))
+
+app.listen(3000);
