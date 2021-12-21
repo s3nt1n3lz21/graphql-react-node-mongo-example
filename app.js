d74f7cbf6102781e0d1ab7b2d39270f6587e953c
Neil Smith <s3nt1n3lz18@gmail.com>
2021-12-21 13:35:05 +0000

    Setting up GraphQL

--- app.js
+++ app.js
@@ -1,14 +1,77 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
-    schema: null,
-    rootValue: {}
+// ! means required, cannot return a null value. A list of null values or just a null response
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+            // Save to MongoDB
+            return event.save()
+                .then(result => {
+                    console.log(result);
+                    return { ...result._doc };
+                })
+                .catch(err => {
+                    console.error(err);
+                    throw err;
+                });
+        }
+    },
+    graphiql: true
 }))
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@cluster0.a8cjy.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(3000);
+})
+.catch(err => {
+    console.error(err);
+});
